374b67a0a110f09325cb2ddb894ee2a26dc85b75
Kathryn Meehan <kcmeehan@ucdavis.edu>
2016-01-14 15:48:43 -0800

    added macro to run eventQAmaker.cxx

---
+++ macros/RunEventQA.C
@@ -0,0 +1,18 @@
+//This macro runs the compiled eventQAmaker binary.
+//The source code for eventQAmaker.cxx can be found at
+//  src/qa/eventQAmaker.cxx
+
+void RunEventQA(TString inputFile, Bool_t eventCuts){
+
+		//Load the Necessary Libraries
+		gSystem->Load("../bin/TrackInfo_cxx.so");
+    gSystem->Load("../bin/PrimaryVertexInfo_cxx.so");
+    gSystem->Load("../bin/EventInfo_cxx.so");
+    gSystem->Load("../bin/ParticleInfo_cxx.so");
+  
+    gSystem->Load("../bin/UserCuts_cxx.so");
+    gSystem->Load("../bin/eventQA_cxx.so");
+
+    eventQAmaker(inputFile,eventCuts);
+
+}
