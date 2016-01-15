7d28d386944c9c3ade10bdfbc23030953dd35cf4
Kathryn Meehan <kcmeehan@ucdavis.edu>
2016-01-15 11:23:15 -0800

    first draft of vertexQA macro

---
+++ macros/RunVertexQA.C
@@ -0,0 +1,18 @@
+//This macro runs the compiled eventQAmaker binary.
+//The source code for eventQAmaker.cxx can be found at
+//  src/qa/eventQAmaker.cxx
+
+void RunVertexQA(TString inputFile, TString outputFile, Bool_t eventCuts, Int_t nEvents){
+
+		//Load the Necessary Libraries
+		gSystem->Load("../bin/TrackInfo_cxx.so");
+    gSystem->Load("../bin/PrimaryVertexInfo_cxx.so");
+    gSystem->Load("../bin/EventInfo_cxx.so");
+    gSystem->Load("../bin/ParticleInfo_cxx.so");
+  
+    gSystem->Load("../bin/UserCuts_cxx.so");
+    gSystem->Load("../bin/vertexQAmaker_cxx.so");
+
+    vertexQAmaker(inputFile,outputFile,eventCuts,nEvents);
+
+}
